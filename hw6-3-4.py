7c0970399abaf34eb62559674ccdfff32572ce98
Ryan Stewart <P22rstewart@fairfieldprep.org>
2021-11-16 07:46:03 -0500

    hw, 6-3-4 was confusing

--- hw6-3-4.py
+++ hw6-3-4.py
@@ -1,2 +1,13 @@
 # Author RTS 11/12/21
-lst = list(input("Enter a list of #s or letters seprated by spaces"))
+lst = list(input("Enter a list of #s or letters seprated by spaces: "))
+lst_e = lst[::2]
+print(lst_e)
+lst1 = int(lst_e.count())
+lst2 = lst1 / 3
+mid = lst2 * 2
+end = lst2 * 3
+print(lst2)
+#lst_1 = lst_e[:1]
+
+#lst_e = (lst[:] % 2 == 0)
+#print(lst_e)
